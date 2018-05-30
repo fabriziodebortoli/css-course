33e076f99939ddba351ea16a1d40eea25fe17b96
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-05-30 17:25:04 +0200

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { Page5Component } from './page5/page5.component';
 
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'page-1', component: Page1Component },
+  { path: 'page-2', component: Page2Component },
+  { path: 'page-3', component: Page3Component },
+  { path: 'page-4', component: Page4Component },
+  { path: 'page-5', component: Page5Component },
+  { path: '**', component: AppComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component,
+    Page5Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
